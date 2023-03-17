d408de1f57053d0d916f70a5f989b36249a0cd51
kahosan <15116355657l@gmail.com>
2023-03-17 16:42:50 +0800

    feat: support `basePath`

--- generate.test.js
+++ generate.test.js
@@ -7,3 +7,8 @@ test("generate routes for example project", () => {
   execSync("cd example && npm run export");
   expect(readFileSync("./example/next-routes.conf", "utf8")).toMatchSnapshot();
 });
+
+test("generate custom routes for example project", () => {
+  execSync("cd example && export BASE_PATH=/next && npm run export");
+  expect(readFileSync("./example/next-routes.conf", "utf8")).toMatchSnapshot();
+});
